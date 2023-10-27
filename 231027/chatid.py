cb7048418fab21fbdc14215ee7d6ceb171fbc228
omoknooni <hihm0609@gmail.com>
2023-10-27 17:48:05 +0900

    231027: homework #1 - timezone fix

--- 231027/chatid.py
+++ 231027/chatid.py
@@ -1,20 +1,19 @@
 import telegram, asyncio, aioschedule
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
 token = ""
 public_chat_name = ""
 chat_id = ""
 
 bot = telegram.Bot(token=token)
-stamp = datetime.now()
 
 # asyncio.run(bot.send_message(chat_id=chat_id, text=f"{stamp.year} / {stamp.month} / {stamp.day} {stamp.hour}:{stamp.minute}:{stamp.second}"))
 async def job():
-    stamp = datetime.now()
+    stamp = datetime.now(timezone(timedelta(hours=9)))
     await bot.send_message(chat_id=chat_id, text=f"{stamp.year} / {stamp.month} / {stamp.day} {stamp.hour}:{stamp.minute}:{stamp.second}")
     print(stamp)
 
-aioschedule.every(30).seconds.do(job)
+aioschedule.every(30).minutes.do(job)
 
 loop = asyncio.get_event_loop()
 while True:
